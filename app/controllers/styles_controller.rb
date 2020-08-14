ded905fce3e3ee18cd4ee0204c76dfa5d2424558
roman9891 <roman9891@gmail.com>
2020-08-14 18:28:16 -0400

    added user relationship and style controller actions

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -1,2 +1,22 @@
 class StylesController < ApplicationController
+    def index
+        styles = Style.all 
+        render json: styles
+    end
+
+    def show
+        style = Style.find(params[:id])
+        render json: style
+    end
+
+    def create
+        style = Style.create(style_params)
+        render json: style
+    end
+
+    private
+
+    def style_params
+        params.require(:styles).permit(:name, :user_id, :properties)
+    end
 end
